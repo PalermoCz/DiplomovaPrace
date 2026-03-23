4ee7e572d68a421cefd806974ede3604ed9ce491
PalermoCz <leggycz@seznam.cz>
2026-03-23 22:00:36 +0100

    Přidáno .agents a ready na Antigravity

--- DiplomovaPrace/Program.cs
+++ DiplomovaPrace/Program.cs
@@ -36,6 +36,8 @@ if (!app.Environment.IsDevelopment())
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
 
+app.UseStaticFiles();
+app.UseRouting();
 app.UseAntiforgery();
 
 app.MapStaticAssets();
